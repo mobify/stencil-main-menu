837e71d10bf295e66f77dd73330767869461dc76
Ryan Frederick <ryan@ryanfrederick.com>
2015-04-23 13:36:16 -0700

    Get bellows working.

--- tests/visual/config.js
+++ tests/visual/config.js
@@ -6,11 +6,15 @@ require.config({
         },
     ],
     paths: {
+        '$': '../../bower_components/jquery/dist/jquery',
         'text': '../../bower_components/text/text',
         'dust-core': '../../bower_components/dustjs-linkedin/dist/dust-core',
         'dust-full': '../../bower_components/dustjs-linkedin/dist/dust-full',
         'dustjs-component': '../../bower_components/dustjs-component',
         'stencil-spec': '../../bower_components/stencil-spec',
+        'plugin': '../../bower_components/plugin/dist/plugin',
+        'velocity': '../../bower_components/mobify-velocity/velocity',
+        'bellows': '../../bower_components/bellows/dist/bellows'
     },
     shim: {
         'dust-core': {
@@ -18,6 +22,9 @@ require.config({
         },
         'dust-full': {
             'exports': 'dust'
+        },
+        '$': {
+            exports: '$'
         }
     },
 });
