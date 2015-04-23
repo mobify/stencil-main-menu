767ba73494c3317779d76d3d1b0fa4890fbebaf6
Ryan Frederick <ryan@ryanfrederick.com>
2015-04-23 12:25:39 -0700

    Initial pass.

---
+++ main-menu.data.js
@@ -0,0 +1,236 @@
+var context = {
+    menuItems: [
+        {
+            "label": "Airing Now",
+            "href": "http://www.jtv.com/broadcast",
+            "items": [
+                {
+                    "label": "Watch Us Live",
+                    "href": "http://www.jtv.com/on/demandware.store/Sites-jtv-Site/default/LiveViewer-Show"
+                },
+                {
+                    "label": "Recent Items",
+                    "href": "http://www.jtv.com/broadcast"
+                },
+                {
+                    "label": "Weekly Product Recap",
+                    "href": "http://www.jtv.com/on/demandware.store/Sites-jtv-Site/default/ProgramGuide-Archive"
+                },
+                {
+                    "label": "Program Guide",
+                    "href": "http://www.jtv.com/on/demandware.store/Sites-jtv-Site/default/ProgramGuide-Show"
+                },
+                {
+                    "label": "Channel Finder",
+                    "href": "http://www.jtv.com/on/demandware.store/Sites-jtv-Site/default/ChannelFinder-Show"
+                },
+                {
+                    "label": "Meet Our Hosts",
+                    "href": "http://www.jtv.com/linkpage?cid=meet-our-hosts"
+                }
+            ]
+        },
+        {
+            "label": "Jewelry",
+            "href":"http://www.jtv.com/jewelry",
+            "items": [
+                {
+                    "label": "Shop by Type",
+                    "href": "http://www.jtv.com/jewelry",
+                    "items": [
+                        {
+                            "label": "Rings",
+                            "href": "http://www.jtv.com/jewelry-rings",
+                            "items": [
+                                {
+                                  "label": "Rings",
+                                  "href": "http://www.jtv.com/jewelry-rings"
+                                },
+                                {
+                                  "label": "Earrings",
+                                  "href": "http://www.jtv.com/jewelry-earrings"
+                                },
+                                {
+                                  "label": "Bracelets",
+                                  "href": "http://www.jtv.com/jewelry-bracelets"
+                                },
+                                {
+                                  "label": "Pendants",
+                                  "href": "http://www.jtv.com/jewelry-pendants"
+                                },
+                                {
+                                  "label": "Necklaces",
+                                  "href": "http://www.jtv.com/jewelry-necklaces"
+                                },
+                                {
+                                  "label": "Watches",
+                                  "href": "http://www.jtv.com/jewelry-watches"
+                                },
+                                {
+                                  "label": "Jewelry Sets",
+                                  "href": "http://www.jtv.com/jewelry-jewelry-sets"
+                                },
+                                {
+                                  "label": "Gemstone Settings",
+                                  "href": "http://www.jtv.com/gemstones/gemstone-settings/settings"
+                                },
+                                {
+                                  "label": "New Jewelry",
+                                  "href": "http://www.jtv.com/shop/specialty/new-arrivals/new_arrivals?prefn1=RETAIL_CATEGORY&prefv1=Jewelry"
+                                },
+                                {
+                                  "label": "Jewelry Sale",
+                                  "href": "http://www.jtv.com/shop/jtv-deals/jtv_deals?prefn1=RETAIL_CATEGORY&prefv1=Jewelry"
+                                },
+                                {
+                                  "label": "All Jewelry",
+                                  "href": "http://www.jtv.com/jewelry?showProducts=1"
+                                }
+                            ]
+                        },
+                        {
+                            "label": "Earrings",
+                            "href": "http://www.jtv.com/jewelry-earrings"
+                        },
+                        {
+                            "label": "Bracelets",
+                            "href": "http://www.jtv.com/jewelry-bracelets"
+                        },
+                        {
+                            "label": "Pendants",
+                            "href": "http://www.jtv.com/jewelry-pendants"
+                        },
+                        {
+                            "label": "Necklaces",
+                            "href": "http://www.jtv.com/jewelry-necklaces"
+                        },
+                        {
+                            "label": "Watches",
+                            "href": "http://www.jtv.com/jewelry-watches"
+                        },
+                        {
+                            "label": "Jewelry Sets",
+                            "href": "http://www.jtv.com/jewelry-jewelry-sets"
+                        },
+                        {
+                            "label": "Gemstone Settings",
+                            "href": "http://www.jtv.com/gemstones/gemstone-settings/settings"
+                        },
+                        {
+                            "label": "New Jewelry",
+                            "href": "http://www.jtv.com/shop/specialty/new-arrivals/new_arrivals?prefn1=RETAIL_CATEGORY&prefv1=Jewelry"
+                        },
+                        {
+                            "label": "Jewelry Sale",
+                            "href": "http://www.jtv.com/shop/jtv-deals/jtv_deals?prefn1=RETAIL_CATEGORY&prefv1=Jewelry"
+                        },
+                        {
+                            "label": "All Jewelry",
+                            "href": "http://www.jtv.com/jewelry?showProducts=1"
+                        }
+                    ]
+                },
+                {
+                    "label": "Shop by Gemstone",
+                    "href": "http://www.jtv.com/jewelry",
+                    "items": [
+                        {
+                            "label": "Rings",
+                            "href": "http://www.jtv.com/jewelry-rings"
+                        },
+                        {
+                            "label": "Earrings",
+                            "href": "http://www.jtv.com/jewelry-earrings"
+                        },
+                        {
+                            "label": "Bracelets",
+                            "href": "http://www.jtv.com/jewelry-bracelets"
+                        },
+                        {
+                            "label": "Pendants",
+                            "href": "http://www.jtv.com/jewelry-pendants"
+                        },
+                        {
+                            "label": "Necklaces",
+                            "href": "http://www.jtv.com/jewelry-necklaces"
+                        },
+                        {
+                            "label": "Watches",
+                            "href": "http://www.jtv.com/jewelry-watches"
+                        },
+                        {
+                            "label": "Jewelry Sets",
+                            "href": "http://www.jtv.com/jewelry-jewelry-sets"
+                        },
+                        {
+                            "label": "Gemstone Settings",
+                            "href": "http://www.jtv.com/gemstones/gemstone-settings/settings"
+                        },
+                        {
+                            "label": "New Jewelry",
+                            "href": "http://www.jtv.com/shop/specialty/new-arrivals/new_arrivals?prefn1=RETAIL_CATEGORY&prefv1=Jewelry"
+                        },
+                        {
+                            "label": "Jewelry Sale",
+                            "href": "http://www.jtv.com/shop/jtv-deals/jtv_deals?prefn1=RETAIL_CATEGORY&prefv1=Jewelry"
+                        },
+                        {
+                            "label": "All Jewelry",
+                            "href": "http://www.jtv.com/jewelry?showProducts=1"
+                        }
+                    ]
+                },
+                {
+                    "label": "Specialty Stores",
+                    "href": "http://www.jtv.com/jewelry",
+                    "items": [
+                        {
+                            "label": "Rings",
+                            "href": "http://www.jtv.com/jewelry-rings"
+                        },
+                        {
+                            "label": "Earrings",
+                            "href": "http://www.jtv.com/jewelry-earrings"
+                        },
+                        {
+                            "label": "Bracelets",
+                            "href": "http://www.jtv.com/jewelry-bracelets"
+                        },
+                        {
+                            "label": "Pendants",
+                            "href": "http://www.jtv.com/jewelry-pendants"
+                        },
+                        {
+                            "label": "Necklaces",
+                            "href": "http://www.jtv.com/jewelry-necklaces"
+                        },
+                        {
+                            "label": "Watches",
+                            "href": "http://www.jtv.com/jewelry-watches"
+                        },
+                        {
+                            "label": "Jewelry Sets",
+                            "href": "http://www.jtv.com/jewelry-jewelry-sets"
+                        },
+                        {
+                            "label": "Gemstone Settings",
+                            "href": "http://www.jtv.com/gemstones/gemstone-settings/settings"
+                        },
+                        {
+                            "label": "New Jewelry",
+                            "href": "http://www.jtv.com/shop/specialty/new-arrivals/new_arrivals?prefn1=RETAIL_CATEGORY&prefv1=Jewelry"
+                        },
+                        {
+                            "label": "Jewelry Sale",
+                            "href": "http://www.jtv.com/shop/jtv-deals/jtv_deals?prefn1=RETAIL_CATEGORY&prefv1=Jewelry"
+                        },
+                        {
+                            "label": "All Jewelry",
+                            "href": "http://www.jtv.com/jewelry?showProducts=1"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
--- main-menu.template.js
+++ main-menu.template.js
@@ -5,6 +5,10 @@ define(function(require) {
     var dust = require('dustjs-component');
     var text = require('text');
     var mainMenu = require('text!./main-menu.dust');
+    var mainMenuLevel = require('text!./main-menu__level.dust');
+    var mainMenuItem = require('text!./main-menu__item.dust');
 
     dust.loadSource(dust.compileComponent(mainMenu, 'main-menu'));
+    dust.loadSource(dust.compileComponent(mainMenuLevel, 'main-menu__level'));
+    dust.loadSource(dust.compileComponent(mainMenuItem, 'main-menu__item'));
 });
